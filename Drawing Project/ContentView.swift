bc39bb87b05319cdf5fa00e1094dde4b29a0e33c
Leonardo Bilhalva <leobbbilhalva@gmail.com>
2023-11-08 17:06:13 -0300

    Initial Commit

---
+++ Drawing Project/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Drawing Project
+//
+//  Created by Leonardo on 08/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Drawing Project/Drawing_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Drawing_ProjectApp.swift
+//  Drawing Project
+//
+//  Created by Leonardo on 08/11/23.
+//
+
+import SwiftUI
+
+@main
+struct Drawing_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
