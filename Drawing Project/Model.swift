19c9a0b80417e94f4fc4e7f22bc2b041c7a28d04
Leonardo Bilhalva <leobbbilhalva@gmail.com>
2023-11-09 17:56:56 -0300

    feat(Model): created model and added them

--- Drawing Project/Model.swift
+++ Drawing Project/Model.swift
@@ -6,3 +6,16 @@
 //
 
 import Foundation
+
+
+struct Model : Hashable {
+    var id : Int
+    var name : String
+    var modelName : String
+}
+
+var models = [
+    Model(id: 0, name: "Shirt 01", modelName: "t shirt.usdz"),
+    Model(id: 1, name: "Shirt 02", modelName: "t shirt.usdz"),
+    Model(id: 2, name: "Shirt 03", modelName: "t shirt.usdz"),
+]
