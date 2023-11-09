3dc0e7685ecc310079168eafdfd50a23dbe2aa3b
Leonardo Bilhalva <leobbbilhalva@gmail.com>
2023-11-09 17:56:36 -0300

    feat(readme): added object on readme

---
+++ Drawing Project/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  Drawing Project
+//
+//  Created by Leonardo on 09/11/23.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeView()
+}
---
+++ Drawing Project/Model.swift
@@ -0,0 +1,8 @@
+//
+//  Model.swift
+//  Drawing Project
+//
+//  Created by Leonardo on 09/11/23.
+//
+
+import Foundation
