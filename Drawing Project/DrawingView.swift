d9f84f69ffadbc56f0609e46584b3a2937c42117
Leonardo Bilhalva <leobbbilhalva@gmail.com>
2023-11-10 17:41:36 -0300

    at this point, I was able to create extract the path and build a image from it. the solve was a good option, but not what I was aimming to do. I need to incorporate the canvas to the object. next steps I will made something new!!

--- Drawing Project/DrawingView.swift
+++ Drawing Project/DrawingView.swift
@@ -1,7 +1,10 @@
 
 import SwiftUI
+import UIKit
 
 struct DrawingView: View {
+
+    @State private var drawnImage: UIImage?
     
     @State private var lines = [Line]()
     @State private var deletedLines = [Line]()
@@ -15,6 +18,13 @@ struct DrawingView: View {
     var body: some View {
         
         VStack {
+            if let image = drawnImage {
+                  Image(uiImage: image)
+                      .resizable()
+                      .aspectRatio(contentMode: .fit)
+                      .frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height / 5)
+                      .border(Color.black, width: 1) // Adiciona uma borda para visualizar o tamanho da imagem
+            }
             
             HStack {
                 ColorPicker("line color", selection: $selectedColor)
@@ -82,7 +92,13 @@ struct DrawingView: View {
             if let last = lines.last?.points, last.isEmpty {
                 lines.removeLast()
             }
+            
+            // Calcula o rect para desenhar a imagem baseado nas linhas existentes
+            let drawingRect = calculateDrawingRect(forLines: lines, lineWidth: selectedLineWidth)
+            // Gera a imagem a partir das linhas desenhadas
+            drawnImage = drawLinesAsImage(lines: lines, in: drawingRect)
         })
+
         
         )
             
@@ -90,8 +106,64 @@ struct DrawingView: View {
     }
 }
 
-struct DrawingView_Previews: PreviewProvider {
-    static var previews: some View {
-        DrawingView()
+//struct DrawingView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        DrawingView()
+//    }
+//}
+
+func drawLinesAsImage(lines: [Line], in rect: CGRect) -> UIImage? {
+    let renderer = UIGraphicsImageRenderer(bounds: rect)
+    return renderer.image { context in
+        for line in lines {
+            guard let firstPoint = line.points.first else { continue }
+            
+            let path = UIBezierPath()
+            path.move(to: firstPoint)
+            
+            for point in line.points.dropFirst() {
+                path.addLine(to: point)
+            }
+            
+            path.lineWidth = line.lineWidth
+            // Converte CustomColor para UIColor e depois define como cor de stroke
+            UIColor(red: 0,
+                    green: 0,
+                    blue: 0,
+                    alpha: 1).setStroke()
+            
+            path.stroke()
+        }
     }
 }
+
+func calculateDrawingRect(forLines lines: [Line], lineWidth: CGFloat) -> CGRect {
+    guard !lines.isEmpty else {
+        return .zero // Retornando um CGRect zero se não houver linhas
+    }
+    
+    var minX: CGFloat = CGFloat.greatestFiniteMagnitude
+    var maxX: CGFloat = -CGFloat.greatestFiniteMagnitude
+    var minY: CGFloat = CGFloat.greatestFiniteMagnitude
+    var maxY: CGFloat = -CGFloat.greatestFiniteMagnitude
+
+    for line in lines {
+        for point in line.points {
+            minX = min(minX, point.x)
+            maxX = max(maxX, point.x)
+            minY = min(minY, point.y)
+            maxY = max(maxY, point.y)
+        }
+    }
+
+    // Considerar a espessura da linha na área de desenho
+    let padding = lineWidth / 2.0
+    return CGRect(
+        x: minX - padding,
+        y: minY - padding,
+        width: maxX - minX + (padding * 2),
+        height: maxY - minY + (padding * 2)
+    )
+}
+
+
